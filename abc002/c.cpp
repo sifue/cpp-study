46859328e1bc7fcf453d3d9139b1bd1cc5ada79d
sifue <yoshimura@soichiro.org>
2017-06-17 02:17:23 +0900

    abc002 c finished.

---
+++ abc002/c.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cfenv>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int xa, ya, xb, yb, xc, yc;
+    cin >> xa >> ya >> xb >> yb >> xc >> yc;
+
+    int a, b, c, d;
+    a = xb - xa;
+    b = yb - ya;
+    c = xc - xa;
+    d = yc - ya;
+
+    int rect = abs((a * d) - (b * c));
+    double s = rect / 2.0;
+
+    cout << fixed << setprecision(1) << s << endl;
+}
