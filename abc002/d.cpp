2d45fc187f6db023c807768f783ceed282d586d6
sifue <yoshimura@soichiro.org>
2017-06-19 02:43:23 +0900

    AC abc002 d

--- abc002/d.cpp
+++ abc002/d.cpp
@@ -1,79 +1,62 @@
 #include <iostream>
-//#include <iomanip>
-//#include <string>
 #include <set>
 #include <vector>
-//#include <cstdlib>
-//#include <cmath>
-//#include <cfenv>
-//#include <bits/stdc++.h>
 
 using namespace std;
+
 /*
-   1.まずは1人目(1)に注目、その人の派閥の人数を出す
-   2.その人と関係を集める(R-2,3)、この際最大派閥のsetを作る
-   3.Rの人から1人選ぶ(r1つまり2)
-   4.r1の関係から、Rに含まれる人取得(R2-2,3)
-   5.Rの人から1人選ぶ (r2つまり3)
-   6.r2の関係が存在しない者を派閥のsetから取り除く
-   7.setのサイズを数えてその人の派閥の人数を取得
-   8.関係の数のmaxを取得しておく
-   9.これを全員に対して行い、最大数を取る
+   指定された人数の議員の全パターンを調べる
    */
+
+int result = 0;
+set<pair<int, int>> rels;
+
+void dfs(int index, int rest, vector<int> giins) {
+  if (rest == 0) {
+    // チェック処理
+    for (int ga : giins) {
+      for (int gb : giins) {
+        if (ga != gb && rels.count(make_pair(ga, gb)) == 0) {
+          return; // 必要な関係がなかったりしたら終了
+        }
+      }
+    }
+
+    // 必要な関係が全部あるならばresultを更新
+    int count = giins.size();
+    if (count > result) {
+      result = count;
+    }
+    return;
+  }
+
+  int next_index = index + 1;
+  int next_rest = rest - 1;
+  dfs(next_index, next_rest, giins); // index の議員を含めない場合
+  giins.push_back(index);
+  dfs(next_index, next_rest, giins); // index の議員を含める場合
+}
+
 int main() {
   int n, m;
   cin >> n >> m;
-  int max = 1;
 
   if (m == 0) {
-    cout << max << endl;
+    cout << 1 << endl;
     exit(0);
-  }
-  // 初期化
-  int rels[n][n];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (i != j) {
-        rels[i][j] = 0;
-      } else {
-        rels[i][j] = 1;
-      }
-    }
   }
 
   // 関係の読み取り
   for (int i = 0; i < m; i++) {
     int x, y;
     cin >> x >> y;
-    rels[x - 1][y - 1] = 1;
-    rels[y - 1][x - 1] = 1;
+    rels.insert(make_pair(x - 1, y - 1));
+    rels.insert(make_pair(y - 1, x - 1));
   }
 
-  for (int i = 0; i < n; i++) { // 1
-    vector<int> giins;
-    set<int> habatsu;
-    habatsu.insert(i);
-    for (int j = 0; j < n; j++) { // 2
-      if (rels[i][j] == 1) {
-        giins.push_back(j);
-        habatsu.insert(j); // 最大の可能性の派閥を作成
-      }
-    }
+  // 深さ優先探索で全部調べる
+  vector<int> giins;
+  dfs(0, n, giins);
 
-    for (int ga : giins) {   // 3  i で選んだ人の知り合い
-      for (int gb : giins) { // 5  i の関係者の関係者として選んだ人
-        if (ga != gb && rels[ga][gb] == 0) {
-          // 6 もし関係がないなら派閥から取り除く
-          habatsu.erase(gb);
-        }
-      }
-    }
-    int habatsuCount = habatsu.size(); // 7
-    if (habatsuCount > max) {          // 8
-      max = habatsuCount;
-    }
-    giins.clear();
-    habatsu.clear();
-  }
-  cout << max << endl;
+  cout << result << endl;
 }
