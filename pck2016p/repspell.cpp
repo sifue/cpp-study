3995059bb2c63e4cb4671672fa7229a44d54681a
Soichiro Yoshimura <soichiro_yoshimura@dwango.co.jp>
2017-08-20 10:14:22 +0000

    イワシロ通った http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0341

--- pck2016p/repspell.cpp
+++ pck2016p/repspell.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#define MOD 1000000007
+
 using namespace std;
 
 int main() {
@@ -8,6 +11,10 @@ int main() {
   cin >> t >> b;
 
   int P[t.size() + 1][b.size() + 1] = {}; // [i][j]
+
+  for (int i = 0; i < b.size() + 1; i++) { // 左端の初期化
+    P[0][i] = 0; 
+  }
 
   for (int i = 0; i < t.size() + 1; i++) { // 上端の初期化
     P[i][0] = 1; 
@@ -17,12 +24,20 @@ int main() {
     char ti = t.at(i - 1);
     for (int j = 1; j < b.size() + 1; j++) {
       char bj = b.at(j - 1);
-
-      if (ti == bj) {
-        P[i][j] = P[i][j - 1] + P[i - 1][j];
+      if (j <= i) {
+        if (ti == bj) {
+          P[i][j] = (P[i - 1][j - 1] + P[i - 1][j]) % MOD;
+        } else {
+          P[i][j] = P[i - 1][j];
+        }
       } else {
-        P[i][j] = P[i - 1][j];
+        P[i][j] = 0;
       }
+      // DEBUG(i);
+      // DEBUG(j);
+      // DEBUG(ti);
+      // DEBUG(bj);
+      // DEBUG(P[i][j]);
     }
   }
   cout << P[t.size()][b.size()] << endl;
