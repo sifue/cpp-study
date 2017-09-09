103685d8bc74af93388ed87bd8dd95373fbf10b0
sifue <yoshimura@soichiro.org>
2017-09-10 04:29:08 +0900

    INF (1 << 29)を使っていく

--- abc073/d.cpp
+++ abc073/d.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define P(s) cout << s << endl;
 #define DEBUG(x) cout << #x << ": " << x << endl;
+#define INF (1 << 29)
 
 vector<int> rs; // 巡回必要なもの全てRs
 int Cs[201][201] = {0}; // 全パスの距離
@@ -22,7 +23,7 @@ int main() {
   for (int i = 1; i <= N; i++) { 
     for (int j = 1; j <= N; j++) { 
       if (i != j) {
-        Cs[i][j] = INT_MAX;
+        Cs[i][j] = INF;
       }
     }
   } // fill
@@ -48,7 +49,7 @@ int main() {
   for (int k = 1; k <= N; k++) {
     for (int i = 1; i <= N; i++) { 
       for (int j = 1; j <= N; j++) { 
-        if(Cs[i][k] != INT_MAX && Cs[k][j] != INT_MAX) { // オーバーフロー対策!!!!
+        if(Cs[i][k] != INF && Cs[k][j] != INF) { // オーバーフロー対策!!!!
           Cs[i][j] = min(Cs[i][j], Cs[i][k] + Cs[k][j]);
         }
       }
@@ -65,7 +66,7 @@ int main() {
 
   // 2. rsのすべての順列組み合わせを作り、最短距離を使ってそれぞれの全コストを求め、最短を出力
   vector<int> paths = rs;
-  int min = INT_MAX;
+  int min = INF;
   sort(paths.begin(), paths.end()); // next_permutation は sortされている必要があった！
   do {
     int sum = 0;
