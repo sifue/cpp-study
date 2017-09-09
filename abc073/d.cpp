f807215e72ec6ef6e00b77f0ee0d66918f8061a9
sifue <yoshimura@soichiro.org>
2017-09-10 03:51:23 +0900

    解法的にはOKそうだけどWA

--- abc073/d.cpp
+++ abc073/d.cpp
@@ -1,28 +1,88 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <cassert>
+#include <algorithm>
+#include <functional>
+#include <climits>
 
 using namespace std;
 
 #define P(s) cout << s << endl;
 #define DEBUG(x) cout << #x << ": " << x << endl;
 
-int Cs[19901][19901] = {};
+vector<int> rs; // 巡回必要なもの全てRs
+long long Cs[201][201] = {}; // 全パスの距離
 
 int main() {
+
   int N, M, R;
   cin >> N >> M >> R;
-  vector<int> rs;
-  for (int i = 0; i < r; i++) {
+  
+  for (int i = 0; i < R; i++) {
     int r;
     cin >> r;
     rs.push_back(r);
   }
+
+  for (int i = 1; i <= N; i++) { 
+    for (int j = 1; j <= N; j++) { 
+      if (i != j) {
+        Cs[i][j] = LLONG_MAX;
+      }
+    }
+  } // fill
+
   for (int i = 0; i < M; i++) {
     int A, B, C;
     cin >> A >> B>> C;
-    Cs[A][B] = C;
-    Cs[B][A] = C;
+    if (Cs[A][B] > C) {
+      Cs[A][B] = C;
+      Cs[B][A] = C;
+    }
   }
-  cout << nums.size() << endl;
+
+  // cout << "---------" << endl;
+  // for (int i = 1; i <= N; i++) { 
+  //   for (int j = 1; j <= N; j++) { 
+  //     DEBUG(i);
+  //     DEBUG(j);
+  //     DEBUG(Cs[i][j]);
+  //   }
+  // }
+  // cout << "---------" << endl;
+
+  // 1. ワーシャルフロイドですべての頂点の対の最小距離を求める
+  for (int k = 1; k <= N; k++) {
+    for (int i = 1; i <= N; i++) { 
+      for (int j = 1; j <= N; j++) { 
+        Cs[i][j] = min(Cs[i][j], Cs[i][k] + Cs[k][j]);
+      }
+    }
+  }
+
+  // for (int i = 1; i <= N; i++) { 
+  //   for (int j = 1; j <= N; j++) { 
+  //     DEBUG(i);
+  //     DEBUG(j);
+  //     DEBUG(Cs[i][j]);
+  //   }
+  // }
+
+  // 2. rsのすべての順列組み合わせを作り、最短距離を使ってそれぞれの全コストを求め、最短を出力
+  vector<int> paths = rs;
+  long long min = LLONG_MAX;
+  do {
+    long long sum = 0;
+    for (int i = 0; i < R - 1; i++) {
+      int start = paths[i];
+      int end = paths[i + 1];
+      sum = sum + Cs[start][end];
+      // cout << start << "->" << end << " ";
+    }
+    // cout << " : " << sum << endl;
+    if (sum <= min) min = sum;
+  } while (next_permutation(paths.begin(), paths.end()));
+
+  cout << min << endl;
 }
