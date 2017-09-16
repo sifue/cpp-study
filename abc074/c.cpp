e451a2abb7d05fbdd731dd1713ff7e5dc67e99b4
sifue <yoshimura@soichiro.org>
2017-09-16 23:54:47 +0900

    通らないテストケースがわからず...

--- abc074/c.cpp
+++ abc074/c.cpp
@@ -21,26 +21,37 @@ int main() {
   cin >> A >> B >> C >> D >> E >> F;
 
   int max_ai, max_bj, max_ci, max_dj;
-  max_ai = F / A;
-  max_bj = F / B;
-  max_ci = (E * F / 100) / C;
-  max_dj = (E * F / 100) / D;
-
   double max_cons = 0.0;
   int result_water = 0;
   int result_sugar = 0;
 
+  max_ai = F / (A * 100);
+  // DEBUG(max_ai);
   repto (ai, max_ai) {
-    if((A * 100 * ai) > F) break;
+    int water_a = (A * 100 * ai);
+    if(water_a > F) continue;
+    max_bj = (F - water_a) / (B * 100);
+    // DEBUG(max_bj);
     repto (bj, max_bj) {
       int water = (A * 100 * ai) + (B * 100 * bj);
-      if(water > F) break;
+      if(water > F) continue;
+      int max_sugar = water * E / 100;
+      max_ci = max_sugar / C;
+      // DEBUG(max_ci);
       repto (ci, max_ci) {
-        if((C * ci) > (water / 100 * E))  break;
+        int sugar_c = C * ci;
+        if(sugar_c > max_sugar) continue;
+        max_dj = (max_sugar - sugar_c) / D;
+        // DEBUG(max_dj);
         repto (dj, max_dj) {
           int sugar = C * ci + D * dj;
           if((water + sugar) <= F &&  sugar <= (water / 100 * E)) {
             double cons = (double)sugar / (double)(water + sugar);
+            // DEBUG(ai);
+            // DEBUG(bj);
+            // DEBUG(ci);
+            // DEBUG(dj);
+            // DEBUG(cons);
             if (cons > max_cons) {
               max_cons = cons;
               result_water = water;
