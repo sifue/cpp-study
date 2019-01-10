4f80b87b7108c1a8a47ec9e0b1fc8ba5ae7a8489
Soichiro Yoshimura <yoshimura@soichiro.org>
2019-01-10 15:15:08 +0000

    yukicoderにはじめて問題を投稿してみる

---
+++ original/a_gen.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>  // g++ -std=c++14 -o a a.cpp
+
+std::mt19937 mt{ std::random_device{}() };
+
+std::uniform_int_distribution<int> dist(1, 10000);
+
+using namespace std;
+
+typedef pair<int, int> P;
+typedef long long ll;
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define repto(i, n) for(int i = 0; i <= (n); i++)
+#define all(c) (c).begin(), (c).end()
+#define uniq(c) c.erase(unique(all(c)), (c).end())
+#define _1 first
+#define _2 second
+#define pb push_back
+#define INF 1145141919
+#define MOD 1000000007
+#define DEBUG(x) cout << #x << ": " << x << endl;
+
+__attribute__((constructor))
+void initial() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
+
+int main() {
+
+    cout << dist(mt) << " " << dist(mt) << endl;
+}
