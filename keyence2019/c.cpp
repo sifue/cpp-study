774e1763ce6456e524a0536efb637cc1ff66c268
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2019-01-14 09:20:47 +0000

    キーエンスコンのC問題まで解いた

--- keyence2019/c.cpp
+++ keyence2019/c.cpp
@@ -26,6 +26,7 @@ void initial() {
 
 int A[100001];
 int B[100001];
+int C[100001];
 int diffs[100001]; // index => diff
 vector<P> diffIndexes; // 昇順 (diff, index)
 ll sumA = 0;
@@ -38,6 +39,7 @@ int main() {
         ll ai;
         cin >> ai;
         A[i] = ai;
+        C[i] = ai;
         sumA += ai;
     }
     rep(i, N) {
@@ -46,6 +48,7 @@ int main() {
         B[i] = bi;
         sumB += bi;
         ll diff = A[i] - B[i]; // diff Bを基準にしたときのA
+        diffs[i] = diff;
         diffIndexes.pb(make_pair(diff, i));
     }
 
@@ -57,16 +60,48 @@ int main() {
     // ソート
     sort(all(diffIndexes));
 
-    int plusIndex = diffs.size() - 1;
+    int plusIndex = diffIndexes.size() - 1;
     int minusIndex = 0;
+    //DEBUG(plusIndex);
+    //DEBUG(minusIndex);
+    //rep (i, N) {
+    //    DEBUG(diffIndexes[i]._1);
+    //    DEBUG(diffIndexes[i]._2);
+    //}
+    //DEBUG("----------");
 
-    while (diffs[diffIndexes[minusIndex]._2] >= 0) { // minusIndexの場所のdiffが0以上で終了
+    while (diffs[diffIndexes[minusIndex]._2] < 0) { // minusIndexの場所のdiffが0以上で終了
+        int currentMinIndex = diffIndexes[minusIndex]._2;
+        int currentMaxIndex = diffIndexes[plusIndex]._2;
+        int restLack = B[currentMinIndex] - C[currentMinIndex] ;
+        int restExtra = C[currentMaxIndex] - B[currentMaxIndex];
+        //DEBUG(plusIndex);
+        //DEBUG(minusIndex);
+        //DEBUG(currentMinIndex);
+        //DEBUG(currentMaxIndex);
+        //DEBUG(restLack);
+        //DEBUG(restExtra);
+        //DEBUG("----------");
 
+        if (restLack > restExtra) {
+            C[currentMinIndex] += restExtra;
+            C[currentMaxIndex] -= restExtra;
+            plusIndex--;
+        } else if (restExtra > restLack) {
+            C[currentMinIndex] += restLack;
+            C[currentMaxIndex] -= restLack;
+            minusIndex++;
+        } else if (restLack == restExtra) {
+            C[currentMinIndex] += restLack;
+            C[currentMaxIndex] -= restExtra;
+            plusIndex--;
+            minusIndex++;
+        }
     }
 
-
-
-
-
-
+    int result = 0;
+    rep (i, N) {
+        if (A[i] != C[i]) result++;
+    }
+    cout << result << endl;
 }
