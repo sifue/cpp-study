1462fa21a53b987ddc7f754d06dc6502fed22d0f
sifue <yoshimura@soichiro.org>
2019-04-22 16:38:17 +0900

    ABまで

---
+++ abc058/abc058_b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>  // g++ -std=c++14 -o a a.cpp
+
+using namespace std;
+
+typedef pair<int, int> P;
+typedef long long ll;
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define repto(i, n) for(int i = 0; i <= (n); i++)
+#define all(c) (c).begin(), (c).end()
+#define uniq(c) c.erase(unique(all(c)), (c).end())
+#define _1 first
+#define _2 second
+#define pb push_back
+#define INF 1145141919
+#define MOD 1000000007
+#define DEBUG(x) cout << #x << ": " << x << endl;
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+__attribute__((constructor)) void initial() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
+
+int main() {
+    string O, E;
+    cin >> O >> E;
+    rep(i, O.size()) {
+        cout << O[i];
+        if (i != E.size()) {
+            cout << E[i];
+        }
+    }
+    cout << endl;
+}
