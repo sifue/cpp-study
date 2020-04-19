387255a1dd9af79d978164dd0fb00a8b2def237c
Soichiro Yoshimura <yoshimura@soichiro.org>
2020-04-19 22:27:29 +0900

    abc163のDまで

---
+++ abc163/d.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>  // g++ -std=c++14 -o a a.cpp
+
+using namespace std;
+
+typedef pair<int, int> P;
+typedef long long ll;
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define repto(i, n) for(int i = 0; i <= (n); i++)
+#define all(c) (c).begin(), (c).end()
+#define uniq(c) c.erase(unique(all(c)), (c).end())
+#define _1 first
+#define _2 second
+#define pb push_back
+#define INF 1145141919
+#define MOD 1000000007
+#define DEBUG(x) cout << #x << ": " << x << endl;
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+__attribute__((constructor))
+void initial() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
+
+ll sumResult[200002] = {0};
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    // 最初にそこまでの足した数を全部作っておく
+    for (int i = 1; i <= 200002; i++) {
+        sumResult[i] = sumResult[i] + sumResult[i - 1] + i;
+    }
+
+    ll result = 0;
+    int i;
+    for (i = K; i <= (N + 1); i++) { // i: 何個選択するか
+        ll sumMin = sumResult[i - 1]; // 最小の選択
+        ll sumMax = sumResult[N] - sumResult[N - i]; // 最大の選択結果
+        result = (result + ((sumMax - sumMin) + 1)) % MOD;
+    }
+    cout << (result) << endl;
+}
